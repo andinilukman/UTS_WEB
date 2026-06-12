6316511b00129951024a494b3f18f234076ef938
Andini Lukman <andinilukman0820@gmail.com>
2026-06-12 15:14:39 +0800

    feat: implementasi database transaction pada store Product

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Category;
 use App\Models\Product;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class ProductController extends Controller
 {
@@ -17,15 +18,12 @@ class ProductController extends Controller
         $category = $request->category;
 
         $products = Product::with('category')
-
             ->when($search, function ($query) use ($search) {
                 $query->where('nama_produk', 'like', "%{$search}%");
             })
-
             ->when($category, function ($query) use ($category) {
                 $query->where('category_id', $category);
             })
-
             ->latest()
             ->paginate(5);
 
@@ -36,110 +34,139 @@ class ProductController extends Controller
             'categories'
         ));
     }
-    
 
     /**
      * Show the form for creating a new resource.
      */
     public function create()
-{
-    $categories = Category::all();
+    {
+        $categories = Category::all();
 
-    return view('products.create', compact('categories'));
-}
+        return view('products.create', compact('categories'));
+    }
+
     /**
      * Store a newly created resource in storage.
      */
     public function store(Request $request)
-{
-    $request->validate([
-        'category_id' => 'required',
-        'nama_produk' => 'required',
-        'harga' => 'required|numeric',
-        'stok' => 'required|numeric',
-        'merk' => 'required',
-        'deskripsi' => 'required'
-    ]);
+    {
+        $request->validate([
+            'category_id' => 'required',
+            'nama_produk' => 'required',
+            'harga' => 'required|numeric',
+            'stok' => 'required|numeric',
+            'merk' => 'required',
+            'deskripsi' => 'required',
+            'status_produk' => 'required'
+        ]);
 
-    Product::create([
-        'category_id' => $request->category_id,
-        'nama_produk' => $request->nama_produk,
-        'harga' => $request->harga,
-        'stok' => $request->stok,
-        'merk' => $request->merk,
-        'deskripsi' => $request->deskripsi
-    ]);
+        DB::beginTransaction();
 
-    return redirect()
-        ->route('products.index')
-        ->with('success', 'Produk berhasil ditambahkan');
-}
+        try {
+
+            Product::create([
+                'category_id' => $request->category_id,
+                'nama_produk' => $request->nama_produk,
+                'harga' => $request->harga,
+                'stok' => $request->stok,
+                'merk' => $request->merk,
+                'deskripsi' => $request->deskripsi,
+                'status_produk' => $request->status_produk
+            ]);
+
+            DB::commit();
+
+            return redirect()
+                ->route('products.index')
+                ->with('success', 'Produk berhasil ditambahkan');
+
+        } catch (\Exception $e) {
+
+            DB::rollBack();
+
+            return redirect()
+                ->route('products.index')
+                ->with('error', 'Produk gagal ditambahkan');
+        }
+    }
+
     /**
      * Display the specified resource.
      */
     public function show(Product $product)
-{
-    $product->load('category');
+    {
+        $product->load('category');
 
-    return view(
-        'products.show',
-        compact('product')
-    );
-}
+        return view('products.show', compact('product'));
+    }
 
     /**
      * Show the form for editing the specified resource.
      */
-   public function edit(Product $product)
-{
-    $categories = Category::all();
+    public function edit(Product $product)
+    {
+        $categories = Category::all();
 
-    return view(
-        'products.edit',
-        compact('product', 'categories')
-    );
-}
+        return view(
+            'products.edit',
+            compact('product', 'categories')
+        );
+    }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(
-    Request $request,
-    Product $product
-)
-{
-    $request->validate([
-        'category_id' => 'required',
-        'nama_produk' => 'required',
-        'harga' => 'required|numeric',
-        'stok' => 'required|numeric',
-        'merk' => 'required',
-        'deskripsi' => 'required'
-    ]);
+    public function update(Request $request, Product $product)
+    {
+        $request->validate([
+            'category_id' => 'required',
+            'nama_produk' => 'required',
+            'harga' => 'required|numeric',
+            'stok' => 'required|numeric',
+            'merk' => 'required',
+            'deskripsi' => 'required',
+            'status_produk' => 'required'
+        ]);
 
-    $product->update([
-        'category_id' => $request->category_id,
-        'nama_produk' => $request->nama_produk,
-        'harga' => $request->harga,
-        'stok' => $request->stok,
-        'merk' => $request->merk,
-        'deskripsi' => $request->deskripsi
-    ]);
+        DB::beginTransaction();
 
-    return redirect()
-        ->route('products.index')
-        ->with('success', 'Produk berhasil diubah');
-}
+        try {
+
+            $product->update([
+                'category_id' => $request->category_id,
+                'nama_produk' => $request->nama_produk,
+                'harga' => $request->harga,
+                'stok' => $request->stok,
+                'merk' => $request->merk,
+                'deskripsi' => $request->deskripsi,
+                'status_produk' => $request->status_produk
+            ]);
+
+            DB::commit();
+
+            return redirect()
+                ->route('products.index')
+                ->with('success', 'Produk berhasil diubah');
+
+        } catch (\Exception $e) {
+
+            DB::rollBack();
+
+            return redirect()
+                ->route('products.index')
+                ->with('error', 'Produk gagal diubah');
+        }
+    }
 
     /**
      * Remove the specified resource from storage.
      */
     public function destroy(Product $product)
-{
-    $product->delete();
+    {
+        $product->delete();
 
-    return redirect()
-        ->route('products.index')
-        ->with('success', 'Produk berhasil dihapus');
-}
+        return redirect()
+            ->route('products.index')
+            ->with('success', 'Produk berhasil dihapus');
+    }
 }
